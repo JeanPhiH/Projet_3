32bbca969f0667f8ef3122ca3a653816905af73b
JeanPhiH <jp.herfeld@gmail.com>
2024-01-31 14:07:48 +0100

    +branch fetchWork & suppr gallery d'origine & remplacement par gallery de l'API

--- FrontEnd/assets/works.js
+++ FrontEnd/assets/works.js
@@ -2,5 +2,29 @@
 const works = await fetch('http://localhost:5678/api/works').then(res => res.json());
 
 
+function fetchWork(works) {
+	for (let i = 0; i < works.length; i++) {
+
+		// Récupération de l'élément du DOM qui accueillera les réalisations
+		const gallery = document.querySelector(".gallery");
+
+		// chaque réalisation dans une balise figure pour la sémantique
+		const figure = document.createElement("figure");
+		// figure.dataset.id = works[i].id
+		const img = document.createElement("img");
+		img.src = works[i].imageUrl;
+		img.alt = works[i].title;
+		const figcaption = document.createElement("figcaption");
+		figcaption.innerText = works[i].title;
+		
+		// ajout des éléments créés à l'arborescence HTML
+		gallery.appendChild(figure);
+		figure.appendChild(img);
+		figure.appendChild(figcaption);
+	}
+}
+
+fetchWork(works);
+
 
 console.log(works);
