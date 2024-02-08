f7742ced74d3797fe013a7735749720759d6caea
JeanPhiH <jp.herfeld@gmail.com>
2024-02-08 14:08:07 +0100

    réparation bug fetch delete ${i}

--- FrontEnd/assets/works.js
+++ FrontEnd/assets/works.js
@@ -1,4 +1,3 @@
-
 /** CHARGEMENT DU MODE EDITION **********/
 
 // si un token et présent dans le sessionStorage, on execute la fonction loadCreatorInterface()
@@ -7,7 +6,6 @@ if (sessionStorage.getItem("token")) {
 }
 
 function loadCreatorInterface() {
-	
 	let logstate = document.querySelector(".logstate");
 	logstate.innerText = "Logout";
 	// on désactive le lien vers login
@@ -16,21 +14,21 @@ function loadCreatorInterface() {
 	logstate.addEventListener("click", function () {
 		sessionStorage.removeItem("token");
 		window.location.href = "index.html";
-	})
+	});
 	let modeEdition = document.querySelector(".mode-edition");
 	modeEdition.classList.add("active");
 	let modifier = document.querySelector(".modifier");
 	modifier.classList.add("active");
 }
 
-
 /** GENERATION DYNAMIQUE DE GALLERY **********/
 
-const works = await fetch('http://localhost:5678/api/works').then(res => res.json());
+const works = await fetch("http://localhost:5678/api/works").then((res) =>
+	res.json()
+);
 
 function fetchWork(works) {
 	for (let i = 0; i < works.length; i++) {
-
 		// Récupération de l'élément du DOM qui accueillera les réalisations
 		const gallery = document.querySelector(".gallery");
 
@@ -42,7 +40,7 @@ function fetchWork(works) {
 		img.alt = works[i].title;
 		const figcaption = document.createElement("figcaption");
 		figcaption.innerText = works[i].title;
-		
+
 		// ajout des éléments créés à l'arborescence HTML
 		gallery.appendChild(figure);
 		figure.appendChild(img);
@@ -52,11 +50,11 @@ function fetchWork(works) {
 
 fetchWork(works);
 
-
-
 /** AJOUT DYNAMIQUE DES BOUTONS FILTRES **********/
 
-const categories = await fetch('http://localhost:5678/api/categories').then(res => res.json());
+const categories = await fetch("http://localhost:5678/api/categories").then(
+	(res) => res.json()
+);
 
 function fetchCategories() {
 	// Bouton TOUS pour tout afficher
@@ -64,8 +62,8 @@ function fetchCategories() {
 	tousButton.innerText = "Tous";
 	document.querySelector(".filters").appendChild(tousButton);
 	tousButton.addEventListener("click", function () {
-    document.querySelector(".gallery").innerHTML = "";
-    fetchWork(works);
+		document.querySelector(".gallery").innerHTML = "";
+		fetchWork(works);
 	});
 
 	for (let i = 0; i < categories.length; i++) {
@@ -78,7 +76,7 @@ function fetchCategories() {
 		newButton.addEventListener("click", function () {
 			const works_i = works.filter(function (work) {
 				return work.category.name === categories[i].name;
-			})
+			});
 			document.querySelector(".gallery").innerHTML = "";
 			fetchWork(works_i);
 		});
@@ -86,7 +84,6 @@ function fetchCategories() {
 }
 
 fetchCategories();
-
 
 /** AJOUT GALLERY DANS LA MODALE **********/
 
@@ -97,67 +94,63 @@ const croix = document.querySelector(".fa-xmark");
 
 function fetchModale(works) {
 	for (let i = 0; i < works.length; i++) {
-		//on crée l'arboresscence suivante: 
+		//on crée l'arboresscence suivante:
 		//<div class="modaleWork"> <img src="imageUrl"> <div class="trash"> <i id="i" class="fa-solid fa-trash-alt"></i> </div> </div>
 		const modaleWork = document.createElement("div");
-		modaleWork.classList.add(`modaleWork`);
+		modaleWork.classList.add("modaleWork");
 
 		const img = document.createElement("img");
 		img.src = works[i].imageUrl;
 		modaleWork.appendChild(img);
-		
+
 		const divTrash = document.createElement("div");
-		divTrash.classList.add(`trash`);
+		divTrash.classList.add("trash");
+		divTrash.id = `${i}`;
 
 		const iTrash = document.createElement("i");
 		iTrash.classList.add("fa-trash-alt");
 		iTrash.classList.add("fa-solid");
-		iTrash.id = `${i}`;
 
 		divTrash.appendChild(iTrash);
 		modaleWork.appendChild(divTrash);
 		modaleGallery.appendChild(modaleWork);
 
 		// clic sur icone trash id="i" -> suppr works[i]
-		iTrash[id=`${i}`].addEventListener("click", function () {
+		document.getElementById(`${i}`).addEventListener("click", function () {
 			fetch("http://localhost:5678/api/works/" + works[i].id, {
 				method: "DELETE",
 				headers: {
-					"Authorization": "Bearer " + sessionStorage.getItem("token"),
-				}
-			})
-			.then((response) => console.log(response.json()))
+					Authorization: "Bearer " + sessionStorage.getItem("token"),
+				},
+			}).then((res) => res.json());
 			modaleGallery.innerHTML = "";
 			fetchModale(works);
-		})
+		});
 	}
 }
-
 
 // apuie sur le bouton "modifier" -> affiche la modale
 modifier.addEventListener("click", function () {
 	modaleBackground.classList.add("active");
 	modaleGallery.innerHTML = "";
 	fetchModale(works);
-})
+});
 
 // appuie sur la croix -> ferme la modale
 croix.addEventListener("click", function () {
 	modaleBackground.classList.remove("active");
-})
+});
 
 // appuie sur le background -> ferme la modale
 modaleBackground.addEventListener("click", function (event) {
 	if (event.target === modaleBackground) {
-		modaleBackground.classList.remove("active");}
-})
-
+		modaleBackground.classList.remove("active");
+	}
+});
 
 /** AFFICHAGE PAGE UPLOAD **********/
 
 const btnAddPhoto = document.querySelector(".btnAddPhoto");
-btnAddPhoto.addEventListener("click", function () {
-	
-})
+btnAddPhoto.addEventListener("click", function () {});
 
 // console.log(works);
